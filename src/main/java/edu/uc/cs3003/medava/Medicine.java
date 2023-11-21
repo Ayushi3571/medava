7a6e9e7426cbea9c793d12123a586809defce579
Ayushi <84962606+Ayushi3571@users.noreply.github.com>
2023-11-20 19:35:09 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -7,14 +7,6 @@ public abstract class Medicine implements Shippable {
         mMedicineName = medicineName;
     }
 
- public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
-        if (this.minimumTemperature() <= lowTemperature &&
-                highTemperature <= this.maximumTemperature()) {
-            return true;
-        }
-        return false;
-    }
-
     public String getMedicineName() {
         return mMedicineName;
     }
@@ -25,6 +17,14 @@ public abstract class Medicine implements Shippable {
 
     public double maximumTemperature() {
         return 100.0;
+    }
+
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature &&
+                highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
     }
 
     public abstract MedicineSchedule getSchedule();
