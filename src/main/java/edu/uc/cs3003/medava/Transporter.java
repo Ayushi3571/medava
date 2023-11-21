ceb8ce23b9d6de105f9da3fe71f93a4b92317869
Ayushi <84962606+Ayushi3571@users.noreply.github.com>
2023-11-20 19:20:15 -0500

    Update Transporter.java

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,20 @@
 package edu.uc.cs3003.medava;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Transporter {
+
+    {
+    goods = new ArrayList<Medicine>();
+    }
+  
+    private String mTransporterName;
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+  
+    public List<Medicine> goods;
+    public void ship() {
+  // Do some shipping!
+}
 }
