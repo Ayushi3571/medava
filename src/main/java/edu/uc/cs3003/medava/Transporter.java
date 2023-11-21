f26054a8f723103eb3b52db7ed202bd4cffe3e58
Ayushi <84962606+Ayushi3571@users.noreply.github.com>
2023-11-20 19:26:13 -0500

    Update Transporter.java

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -3,17 +3,37 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Transporter {
-
+    // public List<Medicine> goods;
+    private String mTransporterName;
+    private List<Medicine> goods;
+    private double mLowTemperature, mHighTemperature;
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
+    public boolean load(Medicine itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+            goods.add(itemToLoad);
+            return true;
+        }
+        return false;
+    }
+    public Medicine unload() {
+        return goods.remove(0);
+    }
+    public boolean isEmpty() {
+        return goods.isEmpty();
+    }
     {
     goods = new ArrayList<Medicine>();
     }
   
-    private String mTransporterName;
     public String getTransporterName() {
         return mTransporterName;
     }
   
-    public List<Medicine> goods;
     public void ship() {
   // Do some shipping!
 }
